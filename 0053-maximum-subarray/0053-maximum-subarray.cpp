ef4f81ce17d35ad2426781977e03d3156d9f6da4
Andy <88058599+CoolDude937@users.noreply.github.com>
2023-08-27 17:27:28 -0400

    Time: 82 ms (78.96%), Space: 67.7 MB (83.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,22 +5,14 @@ public:
         if (size == 0){
             return 0;
         }
-        //this will help us find subarrays
-        int currentIndex = 0;
         //set currentMax to the first number
         int currentMax = nums[0];
         //set globalMax equal and lets get started
         int globalMax = currentMax;
         for (int i = 1; i < size; i++){
-            if (currentMax <= 0){
-                currentIndex = i;
-            }
 
             currentMax = max(nums[i], currentMax + nums[i]);
-            
-            if (currentMax > globalMax){
-                globalMax = currentMax;
-            }
+            globalMax = max(globalMax, currentMax);
         }
         return globalMax;
     }
